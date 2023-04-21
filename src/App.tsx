055a8ac2d16b7549bb0d74a9842f14d73b5fde2d
chrisvasqm <christianvasquezmelo@gmail.com>
2023-04-20 23:11:44 -0400

    Setup base form

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,32 @@
+import {
+  Button,
+  Container,
+  FormControl,
+  FormLabel,
+  Input,
+  Stack
+} from '@chakra-ui/react';
+
 function App() {
-  <div>App</div>;
+  const handleSubmit = (event: any) => {
+    event.preventDefault();
+    console.log('Submitted');
+  };
+
+  return (
+    <Container p={4}>
+      <form onSubmit={handleSubmit}>
+        <Stack spacing={4}>
+          <FormControl>
+            <FormLabel>Nombre</FormLabel>
+            <Input type='text' />
+          </FormControl>
+
+          <Button type='submit'>Submit</Button>
+        </Stack>
+      </form>
+    </Container>
+  );
 }
 
 export default App;
