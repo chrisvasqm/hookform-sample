e152d13fad924370d26ce4efc191a118178b8c7f
chrisvasqm <christianvasquezmelo@gmail.com>
2023-04-21 09:22:11 -0400

    Add input based validation with Zod

--- src/App.tsx
+++ src/App.tsx
@@ -9,37 +9,38 @@ import {
 } from '@chakra-ui/react';
 import { z } from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
-import { useForm } from 'react-hook-form';
+import { FieldValues, useForm } from 'react-hook-form';
 
 const schema = z.object({
-  name: z.string().min(3).max(10)
+  name: z
+    .string()
+    .min(3, { message: 'Nombre no debe ser menor a 3 caracteres' })
+    .max(10, { message: 'Nombre no debe ser mayor a 10 caracteres' })
 });
 
-interface User {
-  name: string;
-}
+type FormData = z.infer<typeof schema>;
 
 function App() {
   const {
     register,
     handleSubmit,
     formState: { errors }
-  } = useForm<User>({
+  } = useForm<FormData>({
     resolver: zodResolver(schema)
   });
 
-  const onSubmit = (user: User) => {
-    console.log('Submitted', user);
+  const onSubmit = (data: FieldValues) => {
+    console.log('Submitted', data);
   };
 
   return (
     <Container p={4}>
-      <form onSubmit={handleSubmit(user => onSubmit(user))}>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <Stack spacing={4}>
-          <FormControl>
+          <FormControl isInvalid={!!errors?.name}>
             <FormLabel htmlFor='name'>Nombre</FormLabel>
-            <Input {...register('name')} id='name' type='text' />
-            {errors.name && <FormErrorMessage>{errors.name.message}</FormErrorMessage>}
+            <Input {...register('name')} />
+            <FormErrorMessage>{errors?.name?.message}</FormErrorMessage>
           </FormControl>
 
           <Button type='submit'>Submit</Button>
