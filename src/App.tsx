0d7b18dc9bdf98b4ecb35ed329b2c46625b83c9c
chrisvasqm <christianvasquezmelo@gmail.com>
2023-04-20 23:33:21 -0400

    Add zodResolver logic

--- src/App.tsx
+++ src/App.tsx
@@ -2,29 +2,44 @@ import {
   Button,
   Container,
   FormControl,
+  FormErrorMessage,
   FormLabel,
   Input,
   Stack
 } from '@chakra-ui/react';
 import { z } from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
 
 const schema = z.object({
   name: z.string().min(3).max(10)
 });
 
+interface User {
+  name: string;
+}
+
 function App() {
-  const handleSubmit = (event: any) => {
-    event.preventDefault();
-    console.log('Submitted');
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm<User>({
+    resolver: zodResolver(schema)
+  });
+
+  const onSubmit = (user: User) => {
+    console.log('Submitted', user);
   };
 
   return (
     <Container p={4}>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit(user => onSubmit(user))}>
         <Stack spacing={4}>
           <FormControl>
-            <FormLabel>Nombre</FormLabel>
-            <Input type='text' />
+            <FormLabel htmlFor='name'>Nombre</FormLabel>
+            <Input {...register('name')} id='name' type='text' />
+            {errors.name && <FormErrorMessage>{errors.name.message}</FormErrorMessage>}
           </FormControl>
 
           <Button type='submit'>Submit</Button>
